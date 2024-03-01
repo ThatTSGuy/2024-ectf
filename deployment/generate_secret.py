c80ae4622afaa42a703ed90f9ff291388bcbf979
Jack Temko <48391646+ThatTSGuy@users.noreply.github.com>
2024-03-01 08:35:12 -0600

    Create generate_secret.py

---
+++ deployment/generate_secret.py
@@ -0,0 +1,11 @@
+import random
+import sys
+
+nums = []
+for x in range(16):
+    n = random.randint(0, 255)
+    nums.append(hex(n))
+
+hex_str = ", ".join(nums)
+
+print("uint8_t SECRET[] = { " + hex_str + "};", file=sys.stdout)
